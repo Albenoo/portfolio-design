0a809cce37d83fade881dc4c99cc89971aa422ef
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2021-01-05 00:53:29 +0100

    images

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const toggle = document.querySelector(".menu");
 const nav = document.querySelector(".navbar_links");
-const main = document.querySelector(".about");
 toggle.addEventListener("click", () => {
   if (nav.style.display === "none") {
     nav.style.display = "flex";
