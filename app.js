81c4bbf4c68bfda0221d7aaa5680019369c4f305
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2021-01-06 00:19:21 +0100

    design

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const toggle = document.querySelector(".menu");
 const nav = document.querySelector(".navbar_links");
+const newyeareve = new Date().getFullYear();
+console.log(newyeareve);
+let year = document.querySelector("#year");
+console.log(year);
+year.innerHTML = newyeareve;
+
 toggle.addEventListener("click", () => {
   if (nav.style.display === "none") {
     nav.style.display = "flex";
