7b3a0d84542b7f33a34aa7d824fa5c60a23de3a3
Albenis Kërqeli <albeniskerqeli5@gmail.com>
2021-01-09 17:53:25 +0000

    some fixes

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const quotes = [
-  "9. “If opportunity doesn’t knock, build a door",
-  "Strive not to be a success, but rather to be of value",
-  " Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference",
-  " Do not let what you cannot do interfere with what you can do",
-  " Whenever you find yourself on the side of the majority, it is time to pause and reflect",
-  " I haven’t failed. I’ve just found 10,000 ways that won’t wor"
-];
+
 const toggle = document.querySelector(".menu");
 const quotediv = document.querySelector(".quotesdiv");
 const generate = document.querySelector("#cta");
 const nav = document.querySelector(".navbar_links");
 const newyeareve = new Date().getFullYear();
-console.log(newyeareve);
-let year = document.querySelector("#year");
-console.log(year);
-year.innerHTML = newyeareve;
-Updatequote = () => {
-  let randomquote = quotes[Math.floor(Math.random(-1) * quotes.length)];
-  quotediv.textContent = randomquote;
-};
-Updatequote();
-
-setInterval(Updatequote, 5000);
-
 const darktoggle = document.querySelector("#toggle");
+let year = document.querySelector("#year");
+year.innerHTML = newyeareve;
+
 
 toggle.addEventListener("click", () => {
   nav.classList.toggle("navbar-show");
@@ -35,11 +19,7 @@ toggle.addEventListener("click", () => {
   }
   nav.style.transition = "1s linear";
 
-  if (toggle) {
-    console.log("You successfully open navbar");
-  } else {
-    console.log("Try Again!");
-  }
+
 });
 
 window.addEventListener("scroll", () => {
@@ -47,7 +27,6 @@ window.addEventListener("scroll", () => {
   navbar.classList.toggle("sticky", window.scrollY > 0);
 });
 
-console.log("Not very good");
 
 darktoggle.addEventListener("click", () => {
   darktoggle.classList.toggle("fa-toggle-on");
